f61a0961348434ef5d790c1b19018d730c80c2cf
KameKav <53335284+kavneluxedy@users.noreply.github.com>
2022-05-10 00:02:46 +0200

    Add download function to App.js
    At this version, the resizer and download function works properly.

--- src/hooks/useDownload.js
+++ src/hooks/useDownload.js
@@ -1,32 +1,33 @@
 import download from "downloadjs";
 import { useEffect } from "react";
 import useResize from "../hooks/useResize";
+import useOriginal from "./useOriginal";
 
 const Download = () => {
-    localStorage.clear();
+  localStorage.clear();
 
-    console.log(localStorage);
+  const { modifiedImg } = useResize();
+  const { file } = useOriginal();
 
-	const { modifiedImg } = useResize();
+  useEffect(() => {
+    console.log(file);
+    console.log(modifiedImg);
+    downloadHandler(modifiedImg, 'filename', 'image/jpg');
+  }, [modifiedImg, file]);
 
-	useEffect(() => {
-		console.log(modifiedImg);
-	}, [modifiedImg]);
-
-	const downloadHandler = (file, filename = "filename", type) => {
-        console.log('ok');
-		new Promise(
-			() => {
-				if (file) {
-					download(file, filename, type); 
-				}
-			},
-			() => {
-				console.error("Promise Rejected");
-			}
-		);
-	}
-    return { downloadHandler };
+  const downloadHandler = (file, filename = "filename", type) => {
+    new Promise(
+      () => {
+        if (file) {
+          download(file, filename, type);
+        }
+      },
+      () => {
+        console.error("Promise Rejected");
+      }
+    );
+  };
+  return { downloadHandler };
 };
 
 export default Download;
--- src/hooks/useResize.js
+++ src/hooks/useResize.js
@@ -1,58 +1,60 @@
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState, useRef, useMemo } from "react";
 import FileResizer from "react-image-file-resizer";
 
 const useResize = () => {
-	const fileInput = useRef();
-	const [width, setWidth] = useState(null);
-	const [heigth, setHeigth] = useState(null);
-	const [modifiedImg, setModifiedImg] = useState(null);
-	const [loading, setLoading] = useState(null);
-	const [error, setError] = useState(null);
+  const fileInput = useRef();
+  const [width, setWidth] = useState(null);
+  const [heigth, setHeigth] = useState(null);
+  const [modifiedImg, setModifiedImg] = useState(null);
+  const [loading, setLoading] = useState(null);
+  const [error, setError] = useState(null);
 
-	useEffect(() => {
-		resize(fileInput.current, width, heigth);
-	}, [fileInput, width, heigth]);
+  useEffect(() => {
+    if (fileInput.current) {
+      resize(fileInput.current.files[0], width, heigth);
+    }
+  }, [fileInput, width, heigth]);
 
-	async function resize(file, width, heigth) { // ! USE MEMO / CALLBACK
-		setLoading(true);
-		try {
-			FileResizer.imageFileResizer(
-				file, // * file
-				width, // * maxWidth in pixel
-				heigth, // * maxHeight in pixel
-				file.type, // * compressFormat
-				100, // * quality
-				0, // * rotation
-				(uri) => {
-					setModifiedImg({ src: uri });
-					if (localStorage) {
-						localStorage.clear();
-					}
-					localStorage.setItem("src", uri);
-				},
-				"base64" // * outputType
-			);
-			var type = fileInput.current.type.replace("image/", ""); // ! TO DO / CHANGE FILE INPUT
-			// var prefix = fileInput.current.name.replace(/.jpg/ || /.jpeg/ || /.png/ ||
-			// /.gif/, ""); var tmp = fileInput.current.files[0].name.indexOf("."); var
-			// suffix = fileInput.current.files[0].name.substr(tmp); var fileName = prefix +
-			// "_" + "UserChoice" + suffix;
-		} catch (err) {
-			setError(err);
-		} finally {
-			setLoading(false);
-			setError(false);
-		}
-	}
-
-	return {
-		setWidth,
-		setHeigth,
-		modifiedImg,
-		loading,
-		error,
-		fileInput,
-	};
+  const resize = (file, width, heigth) => {
+    setLoading(true);
+    try {
+      FileResizer.imageFileResizer(
+        file, // * file
+        width, // * maxWidth in pixel
+        heigth, // * maxHeight in pixel
+        file.type, // * compressFormat
+        100, // * quality
+        0, // * rotation
+        (uri) => {
+          setModifiedImg(uri);
+          if (localStorage) {
+            localStorage.clear();
+          }
+          localStorage.setItem("src", uri);
+        },
+        "base64" // * outputType
+      );
+      // var type = fileInput.current.type.replace("image/", ""); // ! TO DO / CHANGE FILE INPUT
+      // var prefix = fileInput.current.name.replace(/.jpg/ || /.jpeg/ || /.png/ ||
+      // /.gif/, ""); var tmp = fileInput.current.files[0].name.indexOf("."); var
+      // suffix = fileInput.current.files[0].name.substr(tmp); var fileName = prefix +
+      // "_" + "UserChoice" + suffix;
+    } catch (err) {
+      setError(err);
+    } finally {
+      setLoading(false);
+      setError(false);
+    }
+  };
+  return {
+    setWidth,
+    setHeigth,
+    modifiedImg,
+    setModifiedImg,
+    loading,
+    error,
+    fileInput,
+  };
 };
 
 export default useResize;
